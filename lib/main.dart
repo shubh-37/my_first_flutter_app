1f7733d3a1d26dc13c009c556ccce55ad166ba41
Shubh Arya <shubh.arya37@gmail.com>
2024-01-22 00:51:47 +0530

    first hello world program

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
+import "package:flutter/material.dart";
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.deepPurple, Colors.deepOrange],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
+          child: const Center(
+            child: Text(
+              "Hello World!",
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
