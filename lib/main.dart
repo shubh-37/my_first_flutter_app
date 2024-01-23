14d008c50fa3e35cf7c6a57cbc802d5f2c60e46c
Shubh Arya <shubh.arya37@gmail.com>
2024-01-23 07:14:03 +0530

    passed list of colors for dynamcism

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,12 @@ import "package:flutter/material.dart";
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(body: GradientContainer()),
+      home: Scaffold(
+        body: GradientContainer(colors: [
+          Color.fromARGB(255, 32, 6, 77),
+          Color.fromARGB(255, 255, 87, 34)
+        ]),
+      ),
     ),
   );
 }
